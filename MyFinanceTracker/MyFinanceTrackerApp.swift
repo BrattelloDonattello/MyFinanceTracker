46a39e85012f0f8d40c54906359a619dd04c77d0
Ciupac Cristian <cristian.ciupac@ebs-integrator.com>
2024-11-27 20:36:25 +0200

    Init project

---
+++ MyFinanceTracker/MyFinanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFinanceTrackerApp.swift
+//  MyFinanceTracker
+//
+//  Created by Ciupac Cristian  on 27.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyFinanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
